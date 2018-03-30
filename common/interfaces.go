a3047df88c0a5817137231f45a40537a653f72a9
屈文斌 <twfx7758@hotmail.com>
2018-03-30 17:29:33 +0800

    定时分配任务

--- common/interfaces.go
+++ common/interfaces.go
@@ -1,4 +1,4 @@
-package main
+package common
 
 type TaskJobs interface {
 	Exec() error
--- server/server_job.go
+++ server/server_job.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"quwb_jobs/config"
+	"quwb_jobs/common"
 	"log"
 	"database/sql"
 	"time"
-	"quwb_jobs/server/task"
+	"quwb_jobs/task"
 	"fmt"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func main() {
-	c, err := config.ReadConf("./config.json")
+	c, err := common.ReadConf("./config.json")
 	if err != nil {
 		log.Println(err)
 		panic(err)
 	}
 
-	if db, err := sql.Open("adodb", c.Db["sqlserver"]); err == nil{
+	//连接字符串
+	//connString := fmt.Sprintf("server=%s;port%d;database=%s;user id=%s;password=%s", server, port, database, user, password)
+
+	if db, err := sql.Open("mssql", c.Db["sqlserver"]); err == nil{
 		defer db.Close()
 
 		fmt.Println("===========================循环开始===========================")
+		t := task.NewAllocShop(db)
 		for{
-
-			time.Sleep(10 * time.Second)
-
-			t := task.NewAllocShop(db)
-
-			go t.Exec()
+			time.Sleep(5 * time.Minute)
+			t.Exec()
 		}
 		fmt.Println("===========================循环结束===========================")
 	}else{
