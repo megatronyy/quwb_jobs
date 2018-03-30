eb06f3473c1af72b124ab1d21c9c1e92203f2f0b
屈文斌 <twfx7758@hotmail.com>
2018-03-30 17:50:27 +0800

    定时分配任务

--- server/server_job.go
+++ server/server_job.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-	"quwb_jobs/common"
-	"log"
-	"database/sql"
-	"time"
-	"quwb_jobs/task"
+	//"quwb_jobs/common"
+	//"log"
+	//"database/sql"
+	//"time"
+	//"quwb_jobs/task"
 	"fmt"
 
-	_ "github.com/denisenkom/go-mssqldb"
+	//_ "github.com/denisenkom/go-mssqldb"
+	"github.com/robfig/cron"
 )
 
 func main() {
-	c, err := common.ReadConf("./config.json")
+	isStop := make(chan bool)
+	/*c, err := common.ReadConf("./config.json")
 	if err != nil {
 		log.Println(err)
 		panic(err)
-	}
+	}*/
 
+	i := 0
+	c := cron.New()
+	spec := "*/5 * * * * ?"
+	c.AddFunc(spec, func() {
+		fmt.Println("===========================分配任务开始===========================")
+		if i==5{
+			isStop <- true
+		}
+		i++
+		fmt.Println("===========================分配任务结束===========================")
+	})
+
+	c.Start()
+
+	<-isStop
 	//连接字符串
 	//connString := fmt.Sprintf("server=%s;port%d;database=%s;user id=%s;password=%s", server, port, database, user, password)
 
-	if db, err := sql.Open("mssql", c.Db["sqlserver"]); err == nil{
+	/*if db, err := sql.Open("mssql", c.Db["sqlserver"]); err == nil{
 		defer db.Close()
 
 		fmt.Println("===========================循环开始===========================")
@@ -33,5 +50,5 @@ func main() {
 		fmt.Println("===========================循环结束===========================")
 	}else{
 		log.Println(err)
-	}
+	}*/
 }
